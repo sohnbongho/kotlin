bd7efd5cfcf243dfaf6e5ef406ddf5d5cf1f36ad
bhsohn <hanmuson1@gmail.com>
2024-10-12 15:20:16 +0900

    Get일때도 Post처럼 처리해도 인식이 된다.

--- webflux/src/main/kotlin/dev/fastcampus/webflux/controller/HelloController.kt
+++ webflux/src/main/kotlin/dev/fastcampus/webflux/controller/HelloController.kt
@@ -1,14 +1,22 @@
 package dev.fastcampus.webflux.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController {
 
+    // Get일때는 @Parma을 쓰지 않아도 인식이 된다(스프링에서 지원)
     @GetMapping("/hello")
-    suspend fun index() : String {
-        return "Hello async"
+    suspend fun index(reqHello: ReqHello) : String {
+        return "Hello async ${reqHello}"
     }
-
 }
+
+data class ReqHello(
+    val name: String = "",
+    val age : Int = 0,
+)
