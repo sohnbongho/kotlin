223534292d556a1c8a3785ee1f332d62f95cfb31
bhsohn <hanmuson1@gmail.com>
2024-10-26 13:45:00 +0900

    logger

--- 03.webflux-live/src/main/kotlin/dev/fastcampus/webflux/config/TxidFilter.kt
+++ 03.webflux-live/src/main/kotlin/dev/fastcampus/webflux/config/TxidFilter.kt
@@ -1,22 +1,42 @@
 package dev.fastcampus.webflux.config
 
+import io.micrometer.context.ContextRegistry
 import org.slf4j.MDC
 import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.WebFilter
 import org.springframework.web.server.WebFilterChain
+import reactor.core.publisher.Hooks
 import reactor.core.publisher.Mono
 import java.util.*
+
+private const val KEY_TXID = "txid"
 
 @Component
 @Order(1)
 class TxidFilter : WebFilter {
+
+    init{
+        Hooks.enableAutomaticContextPropagation()
+        ContextRegistry.getInstance().registerThreadLocalAccessor(
+            KEY_TXID,
+            {MDC.get(KEY_TXID)},
+            {value -> MDC.put(KEY_TXID, value)},
+            {MDC.remove(KEY_TXID)}
+
+        )
+    }
+
     override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
 
         var uuid = exchange.request.headers["x-txid"]?.firstOrNull() ?: "${UUID.randomUUID()}"
 
-        MDC.put("txid", uuid)
-        return chain.filter(exchange)
+        MDC.put(KEY_TXID, uuid)
+        return chain.filter(exchange).contextWrite{ ctx ->
+            ctx.put(KEY_TXID, uuid)
+        }.doFinally{
+            MDC.remove(KEY_TXID)
+        }
     }
 }
--- 03.webflux-live/src/main/kotlin/dev/fastcampus/webflux/controller/AdvancedController.kt
+++ 03.webflux-live/src/main/kotlin/dev/fastcampus/webflux/controller/AdvancedController.kt
@@ -1,7 +1,11 @@
 package dev.fastcampus.webflux.controller
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import kotlinx.coroutines.reactor.mono
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.slf4j.MDCContext
 import kotlinx.coroutines.withContext
@@ -9,11 +13,17 @@ import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.reflect.MethodSignature
+import org.slf4j.MDC
 import org.springframework.core.KotlinDetector
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import reactor.core.scheduler.Scheduler
+import reactor.core.scheduler.Schedulers
 import kotlin.coroutines.Continuation
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.coroutines.resume
 
 var logger = KotlinLogging.logger{}
@@ -22,14 +32,27 @@ var logger = KotlinLogging.logger{}
 class AdvancedController {
 
     @GetMapping("/test/txid")
-    fun txtid(){
+    suspend fun txtid(){
 
         logger.debug{"hello txid"}
 
-        //delay(100)
+        delay(100)
+        Mono.fromCallable {
+            logger.debug { "teactor call!" }
+        }.subscribeOn(Schedulers.boundedElastic()).awaitSingle()
+
+        mono {
+            logger.debug { "final test" }
+        }.awaitSingleOrNull()
 
         logger.debug{"end"}
     }
+}
+public fun<T> monoA(
+    context: CoroutineContext = MDCContext(),
+    block: suspend CoroutineScope.() -> T?
+):Mono<T>{
+    return kotlinx.coroutines.reactor.mono(context, block)
 }
 
 @Aspect
@@ -50,12 +73,9 @@ class AdvancedAspectConfig {
 
         return if(KotlinDetector.isSuspendingFunction(method)){
             val continuation = jp.args.last() as Continuation<Any>
-
             val newContext = continuation.context + MDCContext()
             val newContinuation = Continuation<Any>(newContext) { continuation.resume(it)}
-
             val jpArg = jp.args.dropLast(1) + newContinuation
-
             jp.proceed(jpArg.toTypedArray())
         }
         else{
