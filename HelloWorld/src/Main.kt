6dbe9127625ddb98c24f62e3045d60b316ce77ad
bhsohn <hanmuson1@gmail.com>
2025-02-20 22:45:52 +0900

    코틀린 강좌

--- HelloWorld/src/Main.kt
+++ HelloWorld/src/Main.kt
@@ -1,7 +1,14 @@
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 fun main() {
-    val name = "My Kotlin"
+
+    val b  = 1;
+    val c :Int;
+    c = 3;
+
+    val e  = "Hello";
+    val name = "My Kotlin" +" asdasd"
+
     //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
     // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
     println("Hello, $name!")
